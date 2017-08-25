3d9eaaaf47427d670c4993d632bd3b3153b54e9e
dogansalman <dogan.salman@yahoo.com>
2017-08-25 17:55:09 +0300

    updates

--- src/app/stockcards/stockcards.component.ts
+++ src/app/stockcards/stockcards.component.ts
@@ -1,9 +1,9 @@
 import {Component, OnInit, TemplateRef } from '@angular/core';
 import {ApiServices} from '../services/api.services';
 import { BsModalService } from 'ngx-bootstrap/modal';
-import { BsModalRef } from 'ngx-bootstrap/modal/modal-options.class';
-import {FormGroup, FormControl} from '@angular/forms';
-
+import {BsModalRef, modalConfigDefaults} from 'ngx-bootstrap/modal/modal-options.class';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   templateUrl: 'stockcards.component.html'
 })
@@ -13,25 +13,41 @@ export class StockcardsComponent implements  OnInit {
   public stockCardList = [];
   public selectedStockCard = {};
   public modalRef: BsModalRef;
-
+  public isAdd: boolean;
   public stockCardAddForm = new FormGroup({
     unit: new FormControl(),
     waybill: new FormControl(),
     not: new FormControl(),
-    suppliers: new FormControl('Seçiniz')
+    supplier: new FormControl('Belirtilmedi')
   })
 
-  constructor(private api: ApiServices, private modalService: BsModalService) { }
+  constructor(private api: ApiServices, private modalService: BsModalService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
     this.api.get('stockcards').subscribe(s => this.stockCardList = s);
   }
 
-  public openModal(template: TemplateRef<any>, stockcard) {
+  public openModal(template: TemplateRef<any>, stockcard, add) {
     this.selectedStockCard = stockcard;
+    this.isAdd = add;
     this.modalRef = this.modalService.show(template, {keyboard: false, ignoreBackdropClick: true});
   }
-  addStock(): void {
+  changeStock(id): void {
+   if (!id) return;
+    const stockMovemenet = Object.assign(this.stockCardAddForm.value, {stockcard_id : id});
+   if (this.isAdd) {
+     this.api.post('stockmovements/add/' + id, stockMovemenet).subscribe( () => {
+       this.stockCardList.find((sc) => sc.id === id).unit += stockMovemenet.unit;
+       this.modalRef.hide();
+       setTimeout(() => this.toastr.success(stockMovemenet.unit + ' adet stok eklendi.'));
+     });
+   } else {
+     this.api.post('stockmovements/remove/' + id, stockMovemenet).subscribe( () => {
+       this.stockCardList.find((sc) => sc.id === id).unit -= stockMovemenet.unit;
+       this.modalRef.hide();
+       setTimeout(() => this.toastr.success(stockMovemenet.unit + ' adet stok çıkartıldı.'));
+     });
+   }
   }
 
 
