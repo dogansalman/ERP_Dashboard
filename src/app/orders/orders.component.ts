5e64d58f371dea61d9f136ba8f136a40cb2cfe6e
dogansalman <dogan.salman@yahoo.com>
2017-10-10 17:38:55 +0300

    update

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -1,18 +1,123 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, TemplateRef} from '@angular/core';
 import {ApiServices} from '../services/api.services';
 import {DatePipe} from '@angular/common';
-
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/modal-options.class';
+import {ToastrService} from 'ngx-toastr';
+import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
 
 @Component({
   templateUrl: 'orders.component.html'
 })
 
-export class OrdersComponent implements  OnInit {
+  export class OrdersComponent implements  OnInit {
   public orderList = [];
+  public personnelList = [];
+  public selectedOrder = {};
+  public modalRef: BsModalRef;
+  /*
+  Production form/
+   */
+  public productionForm: FormGroup;
 
-  constructor(private api: ApiServices) { }
+
+  constructor(private api: ApiServices, private modalService: BsModalService, private toastr: ToastrService, private formBuilder: FormBuilder, private router: Router, private _sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
+    /*
+    Get orders
+     */
     this.api.get('orders').subscribe(s => this.orderList = s);
+
+    /*
+    Get personnels
+   */
+    this.api.get('personnel').subscribe(s => {
+      this.personnelList = s.map(a =>  a.Personnel);
+  });
+
+
+
+    /*
+    Production form validation
+     */
+    this.productionForm = this.formBuilder.group({
+      'name': ['', Validators.required],
+      'description': [null, Validators.required],
+      'is_complate': [false],
+      'is_cancel': [false],
+      'unit': [0, Validators.required],
+      'start_time': ['Seçiniz', Validators.required],
+      'end_time': ['Seçiniz', Validators.required],
+      'updated_date': [],
+      'created_date': [],
+      'production_personnels': this.formBuilder.array([
+        this.initProductionPersonnel()
+      ])
+    });
   }
+
+  autocompleListFormatter = (data: any): SafeHtml => {
+    const html = `<span>${data.name} ${data.lastname} </span>`;
+    return this._sanitizer.bypassSecurityTrustHtml(html);
+  }
+  ValueFormatter(data: any): string {
+    return `${data.name} ${data.lastname}`;
+  }
+
+/*
+Inıt Production Personel
+ */
+  initProductionPersonnel() {
+    return this.formBuilder.group({
+      personel_id: [null, Validators.required]
+    });
+  }
+
+  onPersonnelChange(sc, el): void {
+    el.disabled = true;
+    //console.log(sc);
+  }
+
+  addPersonnelControl() {
+    const control = <FormArray>this.productionForm.controls['production_personnels'];
+    control.push(this.initProductionPersonnel());
+  }
+  removePersonnelControl(i: number) {
+    const control = <FormArray>this.productionForm.controls['production_personnels'];
+    control.removeAt(i);
+
+  }
+  /*
+    create or update order
+   */
+  onSubmit(): void {
+   console.log(this.productionForm.value);
+  }
+
+  /*
+  Open modal
+ */
+  public openModal(template: TemplateRef<any>, order) {
+    this.selectedOrder = order;
+    this.modalRef = this.modalService.show(template, {keyboard: false, ignoreBackdropClick: true, size: 'lg', cssClass: 'deneme'});
+    /*
+    Modal closing
+     */
+    this.modalService.onHide.subscribe((reason: string) => {
+      //this.productionForm.reset();
+    });
+  }
+  /*
+  Navigate detail
+ */
+  navigateDetail(event, id): void {
+    if (!event.target.closest('.btn')) {
+      this.router.navigateByUrl('orders/edit/' + id);
+    }
+  }
+
+
 }
--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -10,11 +10,12 @@ import { TownsModule } from '../shared/directive/towns/towns.module';
 import { CapitalizeModule } from '../shared/pipes/capitalize/capitalize.module';
 import { OrdersRoutingModule } from './orders-routing.module';
 import { CustomersModule } from '../shared/directive/customers/customers.module';
-import { BsDatepickerModule } from 'ngx-bootstrap';
+import {BsDatepickerModule, ModalModule, BsModalRef} from 'ngx-bootstrap';
 import { BsDatepickerActions } from 'ngx-bootstrap/datepicker/reducer/bs-datepicker.actions';
 import { NguiAutoCompleteModule } from '@ngui/auto-complete';
 import { TooltipModule } from 'ngx-bootstrap';
 import { TooltipConfig } from 'ngx-bootstrap';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 
 @NgModule({
   imports: [
@@ -28,14 +29,17 @@ import { TooltipConfig } from 'ngx-bootstrap';
     TooltipModule,
     TownsModule,
     CustomersModule,
+    ModalModule.forRoot(),
     BsDatepickerModule.forRoot(),
-    NguiAutoCompleteModule
+    NguiAutoCompleteModule,
+    TabsModule
   ],
   declarations: [ OrderComponent, OrdersComponent],
   providers: [
     ApiServices,
     BsDatepickerActions,
-    TooltipConfig
+    TooltipConfig,
+    BsModalRef
   ]
 })
 export class OrdersModule { }
